48f8898244685e1d1dda6e7c42769e9ad9e92f11
matheusdearaujo <matheusouzadearaujo@gmail.com>
2023-06-15 22:33:27 -0300

    feat: add home page

---
+++ src/views/pages/Home.js
@@ -0,0 +1,52 @@
+import TopContent from "../components/home/TopContent.js";
+import { useFetch } from "../../utils/index.js";
+
+const Home = {
+  render: async () => {
+    const { data: posts } = await useFetch(
+      "https://5bb634f6695f8d001496c082.mockapi.io/api/posts",
+      { method: "GET" },
+    );
+
+    return `
+      <div>
+        ${await TopContent.render()}
+        <ul>
+          ${posts.map(
+            post => `
+              <div class="pb-4 px-10 flex flex-col justify-center gap-6 md:flex-row">
+                <div class="w-full lg:flex container mx-auto">
+                  <div class="w-full border border-gray-400 lg:border-gray-400 bg-white rounded-b lg:rounded-b-none rounded p-4 flex flex-col justify-between leading-normal">
+                    <div class="mb-4">
+                      <a
+                        href="#/post/${post.id}"
+                        class="text-gray-900 font-bold text-xl hover:underline"
+                      >
+                        ${post.title}
+                      </a>
+                    </div>
+                    <div class="flex items-center">
+                      <div class="text-sm">
+                        <a
+                          class="text-gray-900 leading-none hover:underline"
+                        >
+                          ${post.name}
+                        </a>
+                        <p class="text-gray-600">${new Date(
+                          post.createdAt,
+                        ).toDateString()}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `,
+          )}
+        </ul>
+      </div>
+    `;
+  },
+  afterRender: async () => {},
+};
+
+export default Home;
